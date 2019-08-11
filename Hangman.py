d368c633ca469fbd13e8cb54300a4269467d9022
Rokas Grybenas <hotep303@gmail.com>
2019-08-11 23:03:51 +0300

    Finished program

--- Hangman.py
+++ Hangman.py
@@ -1,97 +1,59 @@
 """A game called Hangman"""
 
 from random import randint
-from game import Game #imports all of the classes within the module in this case just Game class
-instance = Game()
+from game import Game #imports Game class
+image_filepaths = (
+    'text_images\\1.txt', 'text_images\\2.txt', 'text_images\\3.txt', 'text_images\\4.txt', 
+    'text_images\\5.txt', 'text_images\\6.txt', 'text_images\\7.txt', 'text_images\\8.txt'
+    )
 
 #lets create a dictionary with our words from words.txt
 dict_of_words = {}
 dict_key = 0
+players_answer = "Y"
 with open('\\Users\\Lenovo\\Desktop\\GitHUB\\Hangman\\words.txt') as file_object:
     for line in file_object:
         dict_key += 1
         dict_of_words[dict_key] = line.strip()
 
 
-instance.initialize_word(dict_of_words[randint(1, 1)])
-print(instance.word)
-instance.initialize_guess_word()
+print("\nHello and Welcome to 'Hangman'")
+print("\n'Hangman' is best experienced in 'Full Screen' mode")
 
-
-print("Hello and Welcome to 'Hangman'")
-print("'Hangman' is best experienced in 'Full Screen' mode")
-print(instance.guess)
-
-
-
-instance.current_letter = input("Please enter an alphabetical charater you suspect is present in the mystery word:")
-instance.update_guess()
-if instance.correct_letter:
-    print("\nCongratulations, this letter is present")
-    print(instance.guess)
-else:
-    with open('text_images\\1.txt') as file_object:    
-        contents = file_object.read()
-    print(contents)
-    print(instance.guess)
-    
-
-
-instance.current_letter = input("Please enter an alphabetical charater that you suspect is present in the word:")
-instance.update_guess()
-if instance.correct_letter:
-    print("Congratulations, that is a correct letter")
-    print(instance.guess)
-else:
-    with open('text_images\\1.txt') as file_object:    
-        contents = file_object.read()
-    print(contents)
-    print(instance.guess)
-
-
-instance.current_letter = input("Please enter an alphabetical charater that you suspect is present in the word:")
-instance.update_guess()
-if instance.correct_letter:
-    print("Congratulations, that is a correct letter")
-    print(instance.guess)
-else:
-    with open('text_images\\1.txt') as file_object:    
-        contents = file_object.read()
-    print(contents)
-    print(instance.guess)
-
-instance.current_letter = input("Please enter an alphabetical charater that you suspect is present in the word:")
-instance.update_guess()
-if instance.correct_letter:
-    print("Congratulations, that is a correct letter")
-    print(instance.guess)
-else:
-    with open('text_images\\1.txt') as file_object:    
-        contents = file_object.read()
-    print(contents)
-    print(instance.guess)
-
-
-instance.current_letter = input("Please enter an alphabetical charater that you suspect is present in the word:")
-instance.update_guess()
-if instance.correct_letter:
-    print("Congratulations, that is a correct letter")
-    print(instance.guess)
-else:
-    with open('text_images\\1.txt') as file_object:    
-        contents = file_object.read()
-    print(contents)
-    print(instance.guess)
-
-instance.current_letter = input("Please enter an alphabetical charater that you suspect is present in the word:")
-instance.update_guess()
-if instance.correct_letter:
-    print("Congratulations, that is a correct letter")
-    print(instance.guess)
-else:
-    with open('text_images\\1.txt') as file_object:    
-        contents = file_object.read()
-    print(contents)
-    print(instance.guess)
-
-
+intends_to_play = True
+while intends_to_play is True:
+    #Let an instance of the game begin
+    hang_index = 0
+    try:
+        instance = Game()
+        instance.reset_game()
+        random_key = randint(1, 49)
+        instance.initialize_word(dict_of_words[random_key])
+        instance.initialize_guess_word()
+        while hang_index is not 8 or game_won != True:
+            instance.current_letter = input("\nPlease enter an alphabetical charater you suspect is present in the mystery word:")
+            if instance.current_letter.isalpha():
+                instance.update_guess()
+                if instance.correct_letter:
+                    print("\nCongratulations, this letter is present")
+                    print("\n" + " ".join(instance.guess))
+                else:
+                    with open(image_filepaths[hang_index]) as file_object:    
+                        contents = file_object.read()
+                    print(contents)
+                    print(" ".join(instance.guess))
+                    hang_index += 1
+                instance.check_if_game_won()
+                if instance.game_won == True:
+                    print("You have won the game. Praise be!\n")
+                    players_answer = input("If you would fancy another one of em' games, please enter Y, otherwise GG and Farewell:")
+                if (hang_index - 1) == 7:
+                    print("You have been hanged\n")
+                    players_answer = input("If you would fancy another one of em' games, please enter Y, otherwise GG and Farewell:")
+            else:
+                print("\nThe character is missing from the alphabet")
+    except:
+        pass
+    if  players_answer != "Y":
+        intends_to_play = False
+            
--- game.py
+++ game.py
@@ -1,16 +1,15 @@
 """A class that defines an instance of a game"""
 
 class Game:
-    """an instance in this class represents a single game"""
+    """an instance in this class represents a single game and it's attributes"""
 
     def __init__(self):
         """Initialize attributes to describe a game."""
         self.word = []
         self.guess = []
         self.current_letter = ""
-        self.correct_letter = True
-
-        self.odometer_reading = 0
+        self.correct_letter = False
+        self.game_won = False
         
     def initialize_word(self, word):
         for letter in word:
@@ -30,3 +29,18 @@ class Game:
                 self.guess[index] = letter.upper()
                 self.correct_letter = True
             index +=1
+    
+    def check_if_game_won(self):
+        """checks whether the current game has been won"""
+        if self.word == self.guess:
+            self.game_won = True
+        else:
+            self.game_won = False
+    
+    def reset_game(self):
+        """prepares itself for a new game"""
+        self.word = []
+        self.guess = []
+        self.current_letter = ""
+        self.correct_letter = False
+        self.game_won = False
