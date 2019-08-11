2c3e944a4aaf2cc4b95bc265d818567c7d42858f
Rokas Grybenas <hotep303@gmail.com>
2019-08-11 18:42:07 +0300

    Added main game file Hangman.py and a class Game in game.py

---
+++ Hangman.py
@@ -0,0 +1,10 @@
+"""A game called Hangman"""
+
+from game import * #imports all of the classes within the module in this case just Game class
+
+
+
+print("Hello and Welcome, to the Hangman game.")
+import words.txt
+
+
---
+++ game.py
@@ -0,0 +1,39 @@
+"""A class that defines one instance of a game"""
+
+class Game():
+    """an instance in this class represents a single game"""
+
+    def __init__(self):
+        """Initialize attributes to describe a game."""
+        self.game_word =
+
+        self.game_alphabet
+        self.game_word_set 
+
+
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+        
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = str(self.year) + ' ' + self.manufacturer + ' ' + self.model
+        return long_name.title()
+    
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+        
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+    
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
